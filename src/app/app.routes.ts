f0d9a8598e4000259edec78dab0cfac462aa87e1
sladik-maksym <maxim.sl@darly.solutions>
2024-09-12 14:22:17 +0300

    refactor: lazy loading

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,42 +2,67 @@ import { Routes } from '@angular/router';
 
 import { canActivateAuthRoutes } from '@src/app/core/guards/can-activate-auth-routes';
 import { canActivateMainRoutes } from '@src/app/core/guards/can-activate-main-routes';
-import { AuthLayoutComponent } from '@src/app/core/layouts/auth-layout/auth-layout.component';
-import { MainLayoutComponent } from '@src/app/core/layouts/main-layout/main-layout.component';
-import { SignInPageComponent } from '@src/app/pages/auth/sign-in-page/sign-in-page.component';
-import { SignUpPageComponent } from '@src/app/pages/auth/sign-up-page/sign-up-page.component';
-import { DashboardPageComponent } from '@src/app/pages/dashboard-page/dashboard-page.component';
-import { NotFoundPageComponent } from '@src/app/pages/not-found-page/not-found-page.component';
-import { UserPageComponent } from '@src/app/pages/user-page/user-page.component';
 
 export const routes: Routes = [
   {
     path: 'auth',
-    component: AuthLayoutComponent,
+    loadComponent: () =>
+      import('@src/app/core/layouts/auth-layout/auth-layout.component').then(
+        (c) => c.AuthLayoutComponent
+      ),
     children: [
       { path: '', redirectTo: 'sign-in', pathMatch: 'full' },
-      { path: 'sign-in', title: 'SignIn', component: SignInPageComponent },
-      { path: 'sign-up', title: 'SignUp', component: SignUpPageComponent },
+      {
+        path: 'sign-in',
+        title: 'SignIn',
+        loadComponent: () =>
+          import(
+            '@src/app/pages/auth/sign-in-page/sign-in-page.component'
+          ).then((c) => c.SignInPageComponent),
+      },
+      {
+        path: 'sign-up',
+        title: 'SignUp',
+        loadComponent: () =>
+          import(
+            '@src/app/pages/auth/sign-up-page/sign-up-page.component'
+          ).then((c) => c.SignUpPageComponent),
+      },
     ],
     canActivate: [canActivateAuthRoutes],
   },
   {
     path: '',
-    component: MainLayoutComponent,
+    loadComponent: () =>
+      import('@src/app/core/layouts/main-layout/main-layout.component').then(
+        (c) => c.MainLayoutComponent
+      ),
     children: [
       { path: '', redirectTo: 'dashboard', pathMatch: 'full' },
       {
         path: 'dashboard',
         title: 'Dashboard',
-        component: DashboardPageComponent,
+        loadComponent: () =>
+          import('@src/app/pages/dashboard-page/dashboard-page.component').then(
+            (c) => c.DashboardPageComponent
+          ),
       },
       {
         path: 'user/:id',
         title: 'User',
-        component: UserPageComponent,
+        loadComponent: () =>
+          import('@src/app/pages/user-page/user-page.component').then(
+            (c) => c.UserPageComponent
+          ),
       },
     ],
     canActivate: [canActivateMainRoutes],
   },
-  { path: '**', component: NotFoundPageComponent },
+  {
+    path: '**',
+    loadComponent: () =>
+      import('@src/app/pages/not-found-page/not-found-page.component').then(
+        (c) => c.NotFoundPageComponent
+      ),
+  },
 ];
